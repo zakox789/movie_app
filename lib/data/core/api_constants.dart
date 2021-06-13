3a0845d683e4416ca6e415e59dbf4b258074c7d6
zakariya yousuf <zakariyayousuf@zakariyas-MacBook-Pro.local>
2021-06-13 13:26:01 +0530

    file structuring

--- lib/data/core/api_constants.dart
+++
@@ -1,8 +0,0 @@
-class ApiConstants{
-  ApiConstants._();
-  static const String baseUrl='https://api.themoviedb.org/3/';
-  static const String apiKey='5031417129ba64322521d65aa0a557a2';
-  static const String ImageBaseUrl='https://image.tmdb.org/t/p/w500/';
-
-
-}
---
+++ lib/data/data_sources/core/api_costant.dart
@@ -0,0 +1,6 @@
+class ApiConstant{
+  ApiConstant._();
+  static const String BASE_URL= "https://api.themoviedb.org/3/";
+  static const String API_key="5031417129ba64322521d65aa0a557a2";
+  static const String BASE_IMAGE_URL="https://image.tmdb.org/t/p/w500/";
+}
